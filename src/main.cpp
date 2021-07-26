a92159f00174c08815a67bb50236e9c235b34a20
bennydiamond <psyko_chewbacca@hotmail.com>
2021-07-25 23:39:25 -0400

    Reduce ping polling frequency

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 #define MIN_DISTANCE 2 // Minimum distance ping sensor can realisticly measure
 #define MEDIAN_SAMPLE_COUNT (13)
 #define PING_DIFF_VALUE_THRESHOLD (20)
-#define SonarPingLoopPeriod_ms        (25)
+#define SonarPingLoopPeriod_ms        (40)
 #define SonarPingSleepPeriod_ms       (5000)
 #define SonarPingPresenceTimeout_ms   (10000)
 //#define PING_DEBUG_PRINT
